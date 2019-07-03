fe393bbf27c794b147a2f9ddfd05e91e43c275e4
Andrew Maier <Andrew.Maier@eideticom.com>
2019-07-03 15:19:45 -0600

    Only checked the MSI capabilities if MSI is requested

--- Singletons/ctrlrConfig.cpp
+++ Singletons/ctrlrConfig.cpp
@@ -159,19 +159,20 @@ CtrlrConfig::SetIrqScheme(enum nvme_irq_type newIrq, uint16_t numIrqs)
         return false;
     }
     LOG_NRM("Setting IRQ state: %d IRQ(s) of %s", numIrqs, irqDesc.c_str());
+    if (newIrq == INT_MSI_MULTI || newIrq == INT_MSI_SINGLE) {
+        uint64_t current_mc;
+        if (gRegisters->Read(PCISPC_MC, current_mc) == false) {
+            LOG_ERR("Unable to determine MSICAP MC");
+            return false;
+        }
 
-    uint64_t current_mc;
-    if (gRegisters->Read(PCISPC_MC, current_mc) == false) {
-        LOG_ERR("Unable to determine MSICAP MC");
-        return false;
-    }
-
-    uint16_t numMME = 0;
-    for (uint16_t pow2Irq = numIrqs; (pow2Irq & 0x1) == 0; pow2Irq >>= 1, ++numMME);
-    current_mc |= (numMME & 0x7) << 4;
-    if (gRegisters->Write(PCISPC_MC, current_mc) == false) {
-        LOG_ERR("Unable to set MSICAP MC");
-        return false;
+        uint16_t numMME = 0;
+        for (uint16_t pow2Irq = numIrqs; (pow2Irq & 0x1) == 0; pow2Irq >>= 1, ++numMME);
+        current_mc |= (numMME & 0x7) << 4;
+        if (gRegisters->Write(PCISPC_MC, current_mc) == false) {
+            LOG_ERR("Unable to set MSICAP MC");
+            return false;
+        }
     }
 
     struct interrupts state;
